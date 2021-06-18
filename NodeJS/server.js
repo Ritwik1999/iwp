4071c7c8dad4be1c7082db7f4b9cfc27f8d1bead
Ritwik Neema <ritwikneema651@gmail.com>
2021-06-19 02:16:03 +0530

    nodejs initial files

---
+++ NodeJS/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+    console.log(__dirname);
+    console.log("Listening on port 3000");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+    res.status(200);
+    res.setHeader("Content-Type", "text/html");
+    res.send("<h1>Done</h1>");
+});
