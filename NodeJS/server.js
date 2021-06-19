66c4d4485c9e5bf04370b9abbeac8267c39be2c5
Ritwik Neema <ritwikneema651@gmail.com>
2021-06-20 01:16:51 +0530

    ajax jquery added

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -18,3 +18,11 @@ app.post('/quotes', (req, res) => {
     res.setHeader("Content-Type", "text/html");
     res.send("<h1>Done</h1>");
 });
+
+app.get('/jqueryajax', function (req, res) {
+    res.sendFile(__dirname + '/jqueryajax.html');
+})
+
+app.get('/result', function (req, res) {
+    res.sendFile(__dirname + '/result.json');
+});
